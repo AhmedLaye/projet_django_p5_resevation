6a1bc2341e8721c192a52cf7c06a3f3e7e15d498
ahmedlaye <ahmedlaye2001@gmail.com>
2023-05-25 10:05:56 +0000

    reglage menu

--- Booking/views.py
+++ Booking/views.py
@@ -13,19 +13,11 @@ amadeus = Client(
 def index_view(request):
    
     try:
-        response = amadeus.reference_data.locations.hotels.by_city.get(cityCode='DKR')
+        response = amadeus.reference_data.locations.hotels.by_city.get(cityCode='PAR')
         pd.DataFrame(response.data)      
         df = pd.DataFrame(response.data)
         df.to_csv(os.path.join(os.getcwd(), "hotels.csv"), index=False)
     
-        print(response.data)
-        # recuperartion des id des hotels
-        hotelIds = [hotel.get('hotelId') for hotel in response.data]
-     
-        
-        print(f'liste des id : {hotelIds}')
-        print("*****************************************")
-        print(hotel_offers.data)
     except ResponseError as error:
         raise error
     
@@ -67,32 +59,27 @@ def reserver_hotel(request, id):
         if form.is_valid():
             date_arrive = form.cleaned_data['date_arrive']
             date_depart = form.cleaned_data['date_depart']
-            try:
-                # Hotel List API to get list of Hotels by city code
-                # Hotel Search API to get list of offers for a specific hotel
-                hotel_offers = amadeus.shopping.hotel_offers_search.get(
-                    hotelIds=hotel.site_web, adults='2  ', checkInDate=date_arrive, checkOutDate=date_depart)
-                print(f" liste des offres disponible: {hotel_offers.data}")
-                print(hotel_offers.length)
-
+            # try:
+            #     # Hotel List API to get list of Hotels by city code
+            #     # Hotel Search API to get list of offers for a specific hotel
+            #     hotel_offers = amadeus.shopping.hotel_offers_search.get(
+            #         hotelIds=hotel.site_web, adults='1', checkInDate=date_arrive, checkOutDate=date_depart)
+            #     print(f" liste des offres disponible: {hotel_offers.data}")
                 
+            #     # offerId = hotel_offers.data[0]['offers'][0]['id']
 
-                
-                # offerId = hotel_offers.data[0]['offers'][0]['id']
-
-                # guests = [{'id': 1, 'name': {'title': 'MR', 'firstName': 'BOB', 'lastName': 'SMITH'},
-                #         'contact': {'phone': '+33679278416', 'email': 'bob.smith@email.com'}}]
-                # payments = {'id': 1, 'method': 'creditCard', 'card': {
-                #     'vendorCode': 'VI', 'cardNumber': '4151289722471370', 'expiryDate': '2027-08'}}
+            #     # guests = [{'id': 1, 'name': {'title': 'MR', 'firstName': 'BOB', 'lastName': 'SMITH'},
+            #     #         'contact': {'phone': '+33679278416', 'email': 'bob.smith@email.com'}}]
+            #     # payments = {'id': 1, 'method': 'creditCard', 'card': {
+            #     #     'vendorCode': 'VI', 'cardNumber': '4151289722471370', 'expiryDate': '2027-08'}}
                     
-                # hotel_booking = amadeus.booking.hotel_bookings.post(
-                #     offerId, guests, payments)
-                # print(hotel_booking.data)
-               
-               
-            except ResponseError as error:
-                raise error
-                # Hotel booking API to book the offer 
+            #     # hotel_booking = amadeus.booking.hotel_bookings.post(
+            #     #     offerId, guests, payments)
+            #     # print(hotel_booking.data)
+            #     message="succes"
+            # except ResponseError as error:
+            #     raise error
+            #     # Hotel booking API to book the offer 
                 
            
             return render(request, 'booking/hotel_detail.html',{'hotel':hotel, 'message':message, 'date_arrive': date_arrive, 'date_depart': date_depart})
