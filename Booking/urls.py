1f88294155dc5ab92a100e2a7793b82c92be66e6
Aladji-07 <126168025+Aladji-07@users.noreply.github.com>
2023-06-16 08:27:46 +0000

    Rawane (#1)
    * Import
    
    * commo
    
    * commit
    
    * commit
    
    * dernieres modifs
    
    * rsz

--- Booking/urls.py
+++ Booking/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
 from .views  import *
+from Booking import views
 
 urlpatterns =[
     path('', index_view, name='index'),
     path('voiture/', voiture_view, name='voiture'),
     path('vol/', vol_view, name='vol'),
-    path('resto/', resto_view, name='resto')
+    path('resto/', resto_view, name='resto'),
+    path('voiture/<int:car_id>/', detail_voiture_view, name='details') ,
+    path('reservation/', views.car_reservation, name='reservation'),
+    path('register/', views.register, name='register'),
+    path('connection/', views.connection, name='connection'),
+    path('deconnection/', views.deconnection, name='deconnection')
+
     
 ]
--- Booking/views.py
+++ Booking/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404 ,redirect
 from amadeus import Client, ResponseError
 import pandas as pd
 import os
 from Booking.models import Hotels
+from Booking.models import voiture
+from Booking.models import Reservation_voiture
+from Booking.forms import ReservCarForm
+from Booking.models import Utilisateur
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate , login ,logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index_view(request):
     amadeus = Client(
@@ -28,8 +36,9 @@ def index_view(request):
     return render(request,  'booking/index.html')
 
 def voiture_view(request):
+    voitures=voiture.objects.raw("SELECT * FROM Booking_voiture WHERE id IN (1,2,3,4,5,6)")
 
-    return render(request,  'booking/voiture.html')
+    return render(request,  'booking/voiture.html',{'voitures':voitures})
 
 def vol_view(request):
 
@@ -38,3 +47,87 @@ def vol_view(request):
 def resto_view(request):
     hotels = Hotels.objects.raw("SELECT * FROM  Booking_hotels")
     return render(request,  'booking/resto.html', {'hotels':hotels})
+
+def detail_voiture_view(request ,car_id):
+    car = get_object_or_404( voiture, id = car_id)
+
+    return render(request,  'booking/detail_voiture.html' , {'car':car})
+
+@login_required (login_url='connection')
+def car_reservation(request):
+    
+    
+    if request.method == 'POST':
+        
+        date_debut = request.POST['date_debut']  # Date de début de réservation
+        date_fin = request.POST['date_fin']# Date de fin de réservation
+        voiture_id = request.POST['identifiant_voiture']
+        utilisateur_id=request.user.id
+    
+        # Création de l'objet réservation
+        reservation = Reservation_voiture( utilisateur_id=utilisateur_id ,voiture_id=voiture_id,date_debut_location=date_debut, date_fin_location=date_fin)
+        reservation.save()
+        
+        return redirect('voiture')  # Rediriger vers une page de confirmation
+
+    return render(request, 'voiture')
+
+def register(request):
+    if request.method =='POST':
+        nom_utilisateur=request.POST['name']
+        email=request.POST['email']
+        password=request.POST['pass']
+        password1=request.POST['re_pass']
+       
+        if password != password1:
+            message_erreur='les mots de pass ne sont pas conformes'
+            return render(request, 'booking/register.html', {'message_erreur': message_erreur})
+            
+        user= User.objects.create_user(nom_utilisateur,email,password)
+
+        user.save()
+    
+    return render(request, 'booking/register.html')
+
+
+def connection(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['pass']
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None and user.is_active:
+            login(request, user)
+            messages.success(request, 'Connexion réussie ')
+            return redirect('voiture')
+        else:
+            messages.error(request, "Nom d'utilisateur ou mot de passe incorrect")
+     
+    return render(request, 'booking/connection.html')
+
+def deconnection(request):
+    logout(request)
+    return redirect('voiture')
+
+    
+def recherches_voitures(request):
+    if request.method == 'POST':
+        date_debut = request.POST['date_debut']
+        date_fin = request.POST['date_fin']
+
+        # Requête pour récupérer les voitures disponibles
+        voitures_disponibles = voiture.objects.exclude(
+            reservation__date_debut_reservation__lte=date_fin,
+            reservation__date_fin_reservation__gte=date_debut
+        )
+
+        context = {
+            'voitures_disponibles': voitures_disponibles,
+            'date_debut': date_debut,
+            'date_fin': date_fin
+        }
+
+        return render(request, 'results.html', context)
+
+    return render(request, 'search.html')
